e3366c57fb64e3af62f710e1c7fe02702ecc5302
Caio Tracera <ctrcra@gmail.com>
2021-02-23 15:54:21 -0300

    chore: change page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import Head from 'next/head'
+
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
+
+import { Container, LeftContainer, RightContainer } from '../styles/index'
 
 const Home: React.FC = () => {
   return (
     <div className="container">
+      <Head>
+        <title>Início - Move.it</title>
+      </Head>
       <ExperienceBar />
+      <Container>
+        <LeftContainer>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </LeftContainer>
+        <RightContainer>{/* <Profile></Profile> */}</RightContainer>
+      </Container>
     </div>
   )
 }
