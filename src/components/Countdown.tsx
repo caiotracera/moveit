f4e6cb288dd23b413ff8f6c6d107bd4120f83a28
Caio Tracera <ctrcra@gmail.com>
2021-02-23 18:25:55 -0300

    feat: add Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,45 @@
+import { useState, useEffect } from 'react'
 import { Container, CountDownContainer } from '../styles/components/Countdown'
 
 export function Countdown() {
+  const [time, setTime] = useState(25 * 60)
+  const [active, setActive] = useState(false)
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+
+  function startCountdown() {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    }
+  }, [active, time])
+
   return (
     <Container>
       <CountDownContainer>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </CountDownContainer>
 
-      <button type="button">Iniciar um ciclo</button>
+      <button type="button" onClick={startCountdown}>
+        Iniciar um ciclo
+      </button>
     </Container>
   )
 }
