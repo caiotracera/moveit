ec5aaa08df4a31a66264a5d61e2907e33851f138
Caio Tracera <ctrcra@gmail.com>
2021-02-23 15:58:24 -0300

    chore: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,6 +46,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
