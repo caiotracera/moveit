1f60852da8a5f22ead1a1af6366e1c3c891ce9f9
Caio Tracera <ctrcra@gmail.com>
2021-02-23 15:25:33 -0300

    feat: add CompletedChallenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import { Container } from '../styles/components/CompletedChallenges'
+
+export function CompletedChallenges() {
+  return (
+    <Container>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </Container>
+  )
+}
