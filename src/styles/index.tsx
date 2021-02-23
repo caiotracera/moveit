bc7e26037eee465e9eb6e9b400919fbb6c89615e
Caio Tracera <ctrcra@gmail.com>
2021-02-23 18:25:48 -0300

    style: add styles to index

---
+++ src/styles/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  flex: 1;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 6.25rem;
+  align-content: center;
+`
+
+export const LeftContainer = styled.div``
+
+export const RightContainer = styled.div``
