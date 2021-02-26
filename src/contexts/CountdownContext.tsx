163438349d3113190a0ced8cc717951f3ecdf5bf
Caio Tracera <ctrcra@gmail.com>
2021-02-26 12:45:04 -0300

    fix: change time from 6s to 25min

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -27,7 +27,7 @@ let countdownTimeout: NodeJS.Timeout
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext)
 
-  const [time, setTime] = useState(0.1 * 60)
+  const [time, setTime] = useState(25 * 60)
   const [isActive, setIsActive] = useState(false)
   const [hasFinished, setHasFinished] = useState(false)
 
@@ -41,7 +41,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout)
     setIsActive(false)
-    setTime(0.1 * 60)
+    setTime(25 * 60)
     setHasFinished(false)
   }
 
