8bd6990ae2d54c8ee835e2804d78a8e0e513cde5
Caio Tracera <ctrcra@gmail.com>
2021-03-04 18:06:03 -0300

    feat: add chromium to generate screenshot

---
+++ src/pages/api/_lib/chromium.ts
@@ -0,0 +1,35 @@
+import puppeteer, { Page } from 'puppeteer-core'
+
+import { getOptions } from './chromeOptions'
+let _page: Page | null
+
+async function getPage(isDev: boolean): Promise<Page> {
+  if (_page) {
+    return _page
+  }
+
+  const options = await getOptions(isDev)
+  const browser = await puppeteer.launch(options)
+  _page = await browser.newPage()
+
+  return _page
+}
+
+export async function getScreenshot(
+  html: string,
+  isDev: boolean
+): Promise<Buffer> {
+  const page = await getPage(isDev)
+
+  await page.setViewport({ width: 1200, height: 630 })
+  await page.setContent(html)
+  await page.evaluateHandle('document.fonts.ready')
+
+  const file = await page.screenshot({ type: 'png' })
+
+  if (!file) {
+    throw new Error('Error while generating screenshot')
+  } else {
+    return file as Buffer
+  }
+}
